48480456d59a86e6a50063d2e7873ca7e1ffbe4d
kchudzic <ckmt.chudzicki@gmail.com>
2022-10-09 14:24:00 +0200

    Initial commit

---
+++ src/podyplomowe.java
@@ -0,0 +1,16 @@
+public class podyplomowe {
+    public static void main(String[] args) {
+        String name;
+        String type;
+        short year;
+
+        name = "Lol";
+        type = "moba";
+        year = 2010;
+
+        System.out.println("Nazwa gry: "+name);
+        System.out.println("Gatunek gry: "+type);
+        System.out.println("Rok wydania: "+year);
+
+    }
+}
